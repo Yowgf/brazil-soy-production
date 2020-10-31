4c2a7c7da83f1deac9cb45b062c2260a9b92f0ae
Yowgf <alexthomasmol@gmail.com>
2020-10-31 16:07:06 -0300

    Initial version

---
+++ src/lib/__init__.py
@@ -0,0 +1,11 @@
+# Project ICD-TP1 python file
+#
+# lib package
+# __init__ packager
+#
+# Description:
+# This file compiles together the collection of general purpose
+#   modules used by the project.
+################################################################
+
+from .preprocess import *
---
+++ src/lib/native_libs.py
@@ -0,0 +1,11 @@
+# Project ICD-TP1 python file
+#
+# lib package
+# native_libs importer
+#
+# Description:
+# This file imports globally used libraries into the project
+################################################################
+
+import numpy as np
+import pandas as pd
